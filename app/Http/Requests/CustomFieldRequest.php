8723bfe0feb299091c6b271b5a216cd257af9ef5
Michael Lee <michael@dss.com.mm>
2022-05-14 17:54:08 +0630

    request files for validation

---
+++ app/Http/Requests/CustomFieldRequest.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+use Illuminate\Validation\Rule;
+
+class CustomFieldRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        return [
+            //
+            'name'=>'required|unique:fields|max:255',
+            'label' => 'required',
+            'type' => [
+                'required',
+                Rule::in(['text', 'number', 'date']),
+            ],
+        ];
+    }
+
+    protected function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException(response()->json([
+            'status' => 422,
+            'message' => $validator->errors(),
+        ], 422));
+    }
+
+}
---
+++ app/Http/Requests/TemplateRequest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class TemplateRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        return [
+            //
+            'form_name' => 'required|string|max:255',
+        ];
+    }
+}
